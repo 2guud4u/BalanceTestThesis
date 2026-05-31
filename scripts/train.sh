b3beb4ecc150b5f8784a5322313c9460b29469e2
Jia Hua Jiang <jiahua212@gmail.com>
2026-05-30 21:39:18 -0700

    fix: added ASFormer

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,6 @@
-
+# mstcn
 python3 train.py \
     --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
-    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAMP.yml \
-    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
-    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
-
-python3 train.py \
-    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPC.yml \
     --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAMP.yml \
     --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
     --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
@@ -19,12 +13,6 @@ python3 train.py \
 
 python3 train.py \
     --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
-    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAE.yml \
-    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
-    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
-
-python3 train.py \
-    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPC.yml \
     --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAE.yml \
     --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
     --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
@@ -51,5 +39,79 @@ python3 train.py \
     --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
     --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/GCN.yml \
     --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPC.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/MSTCN.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAMP.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAMP.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAE.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/MAE.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/GCN.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPC.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/GCN.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
+    --encoder /code/jjiang23/BalanceTestThesis/configs/encoder/GCN.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MB.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPC.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
+    --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml && \
+
+python3 train.py \
+    --data /code/jjiang23/pathml/aim2_balanceV2/configs/data/MPW.yml \
+    --segmentor /code/jjiang23/BalanceTestThesis/configs/segmentor/biLSTM.yml \
     --trainer /code/jjiang23/BalanceTestThesis/configs/trainer/downsamp.yml
+
+
 
